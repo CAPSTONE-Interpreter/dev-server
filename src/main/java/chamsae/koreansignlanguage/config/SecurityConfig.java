ee707b2ad723678024dfe2eeaa8fffafd777fc55
zzinaa <wlsdk2769@ajou.ac.kr>
2021-05-15 23:55:33 +0900

    Add test url

--- src/main/java/chamsae/koreansignlanguage/config/SecurityConfig.java
+++ src/main/java/chamsae/koreansignlanguage/config/SecurityConfig.java
@@ -14,6 +14,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/videos/**").permitAll()
                 .antMatchers("/members/**").permitAll()
+                .antMatchers("/test/**").permitAll()
         ;
     }
 }
