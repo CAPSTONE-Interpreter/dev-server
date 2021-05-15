99f68b24b0ce8ad9a3a294b6204b8c96335efd2d
zzinaa <wlsdk2769@ajou.ac.kr>
2021-05-15 23:42:09 +0900

    add transactional annotation

--- src/test/java/chamsae/koreansignlanguage/service/MemberServiceIntegrationTest.java
+++ src/test/java/chamsae/koreansignlanguage/service/MemberServiceIntegrationTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
+@Transactional
 public class MemberServiceIntegrationTest {
 
     @Autowired
