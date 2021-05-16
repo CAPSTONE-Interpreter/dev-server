44f0b8a4f67c1912d7e67ce96fdbbe87f786a3f7
zzinaa <wlsdk2769@ajou.ac.kr>
2021-05-17 02:33:48 +0900

    Modify config file

--- src/main/java/chamsae/koreansignlanguage/config/SecurityConfig.java
+++ src/main/java/chamsae/koreansignlanguage/config/SecurityConfig.java
@@ -12,7 +12,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/videos/**").permitAll()
+                .antMatchers("/videos/**", "/translate").permitAll()
                 .antMatchers("/members/**").permitAll()
                 .antMatchers("/test/**").permitAll()
         ;
