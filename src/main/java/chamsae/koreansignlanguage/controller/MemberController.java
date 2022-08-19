81b4b382f513cfc0842d809efe9b67e8c58211ab
zzinaa <wlsdk2769@ajou.ac.kr>
2022-08-19 18:06:34 +0900

    swagger 설정 추가

--- src/main/java/chamsae/koreansignlanguage/controller/MemberController.java
+++ src/main/java/chamsae/koreansignlanguage/controller/MemberController.java
@@ -3,6 +3,9 @@ package chamsae.koreansignlanguage.controller;
 import chamsae.koreansignlanguage.DTO.MemberDTO;
 import chamsae.koreansignlanguage.entity.Member;
 import chamsae.koreansignlanguage.service.MemberService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,26 +15,29 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 
+@Api(tags = {"1. Member"})
 @Slf4j
 @Controller
 public class MemberController {
 
     //TODO
     /*
-    * 회원가입 join
-    * 로그인 logIn
-    * 탈퇴 deleteId
+    * 등록
+    * 조회
+    * 삭제
+    * 정보수정
     */
 
     @Autowired
     private MemberService memberService;
 
-    //회원가입
+    //회원 등록
+    @ApiOperation(value = "회원 등록", notes = "회원을 등록합니다.")
     @PostMapping("members")
-    public ResponseEntity<MemberDTO> join(@RequestBody MemberDTO memberDTO) {
-        log.info("POST /member 실행 - 회원가입 : {}", memberDTO.toString());
+    public ResponseEntity<MemberDTO> registerMem(@ApiParam(value = "회원 정보", required = true) @RequestBody MemberDTO memberDTO) {
+        log.info("POST /members 실행 - 회원등록 : {}", memberDTO.toString());
 
-        //회원가입 실패
+        //등록 실패
         //1. 이미 가입된 메일로 가입 시도
 //        if(!memberService.checkEmail(memberDTO))
 //            return ResponseEntity
@@ -43,8 +49,8 @@ public class MemberController {
 
 
 
-        //회원가입 성공
-        Member mem = memberService.join(memberDTO);
+        //등록 성공
+        Member mem = memberService.registerMem(memberDTO);
         //여기서 Member 객체를 쓰는게 맞나? Entity 는 서비스-디비 단에서 써야 하는거 아닌가?
         //id를 가져와서 uri 를 생성해주려면 Member 객체가 필요하긴 함.. (MemberDTO 에는 id 칼럼 없음)
 
