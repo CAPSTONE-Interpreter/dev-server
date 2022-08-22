ed5d5678cb1a219aadb7e983a44ef4add0de136b
zzinaa <wlsdk2769@ajou.ac.kr>
2022-08-22 11:00:46 +0900

    h2 설정 추가

--- src/main/java/chamsae/koreansignlanguage/config/SecurityConfig.java
+++ src/main/java/chamsae/koreansignlanguage/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package chamsae.koreansignlanguage.config;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -17,9 +18,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         "/translate",
                         "/scrap/**",
                         "/members/**",
-                        "/test/**"
+                        "/test/**",
+                        "h2-console/**"
                 ).permitAll()
 
         ;
+
+        http
+                .headers()
+                .frameOptions()
+                .disable();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+                .ignoring()
+                .antMatchers(
+                        "h2-console/**"
+                );
     }
 }
