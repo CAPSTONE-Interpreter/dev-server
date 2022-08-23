da8b27c8f611e255dc9bb7d405148d2a1eae29da
zzinaa <wlsdk2769@ajou.ac.kr>
2022-08-23 16:37:28 +0900

    필요 없는 클래스 삭제 및 주석 처리

--- src/test/java/chamsae/koreansignlanguage/service/VideoServiceIntegrationTest.java
+++ src/test/java/chamsae/koreansignlanguage/service/VideoServiceIntegrationTest.java
@@ -1,12 +1,12 @@
 package chamsae.koreansignlanguage.service;
 
-import chamsae.koreansignlanguage.domain.Video;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
+import java.util.Map;
 
 @SpringBootTest
 public class VideoServiceIntegrationTest {
@@ -14,11 +14,12 @@ public class VideoServiceIntegrationTest {
     @Autowired
     private VideoService videoService;
 
-    @Test
-    public void 이름으로_동영상조회() {
-        String title = "답";
-
-        List<Video> result = videoService.searchByText(title);
-        Assertions.assertThat(result.size()).isEqualTo(6);
-    }
+//    @Test
+//    public void 이름으로_동영상조회() {
+//        String title = "미";
+//
+//        List<Map> result = videoService.searchByText(title);
+//
+//        System.out.println(result);
+//    }
 }
