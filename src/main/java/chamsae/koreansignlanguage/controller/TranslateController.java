4d83d273f1dc9bad8474345f0144e1ee29989e5b
zzinaa <wlsdk2769@ajou.ac.kr>
2022-08-24 11:42:15 +0900

    수어 번역 컨트롤러 수정

--- src/main/java/chamsae/koreansignlanguage/controller/TranslateController.java
+++ src/main/java/chamsae/koreansignlanguage/controller/TranslateController.java
@@ -1,13 +1,18 @@
 package chamsae.koreansignlanguage.controller;
 
 import chamsae.koreansignlanguage.service.TranslateService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@Api(tags = "4. Translate")
 @Slf4j
 @Controller
 public class TranslateController {
@@ -15,10 +20,13 @@ public class TranslateController {
     @Autowired
     private TranslateService translateService;
 
+    //ㅇㅏㄴㄴㅕㅇ => 안녕
+    @ApiOperation(value = "단어 조합", notes = "요청한 자모음을 합쳐, 단어를 만듭니다.")
     @GetMapping("translate")
-    @ResponseBody
-    public String makeSentence(@RequestParam("word") String word) {
-        log.info("translate 실행 - 단어 조합 : {}", word);
-        return translateService.springToFlask(word);
+    public ResponseEntity<String> makeWord(@ApiParam(value = "자모음", required = true) @RequestParam("word") String word) {
+
+        //단어 조합 성공
+        log.info("GET /translate?word={} 실행 - 단어 조합 : {}", word, word);
+        return ResponseEntity.ok(translateService.springToFlask(word));
     }
 }
