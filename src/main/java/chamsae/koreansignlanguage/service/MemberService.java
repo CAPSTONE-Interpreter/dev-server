d32076263eeac57436dd415dbec2c560a8461a72
zzinaa <wlsdk2769@ajou.ac.kr>
2022-08-29 15:43:51 +0900

    불필요한 코드 삭제 및 수정

--- src/main/java/chamsae/koreansignlanguage/service/MemberService.java
+++ src/main/java/chamsae/koreansignlanguage/service/MemberService.java
@@ -33,39 +33,40 @@ public class MemberService {
 
     //회원 등록
     public Member registerMem(MemberDTO memberDTO) {
-            Member member = mapper.toEntity(memberDTO);
-            return memberRepository.save(member);
+
+        //실패 1. 해당 이메일로 가입된 정보가 있음.
+//        if(!checkEmail(memberDTO))
+            //exception 던지기
+
+        Member member = mapper.toEntity(memberDTO);
+        return memberRepository.save(member);
     }
 
     //회원 조회
     public MemberDTO getMem(long id) {
 
-        Optional<Member> m = memberRepository.findById(id);
-        m.orElseThrow(()->new IllegalArgumentException("해당 ID가 존재하지 않습니다. ID : " + id));
+        //실패 1. 해당 멤버 아이디가 존재하지 않음. 잘못된 접근.
+        Member m = memberRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 ID가 존재하지 않습니다. ID : " + id));
 
-        return mapper.toDto(m.get());
+        return mapper.toDto(m);
     }
 
     //회원 정보 수정
     public MemberDTO updateInfo(long id, MemberDTO memberDTO) {
 
-//        Member m = memberRepository.find //null로 나올때 처리 추가하기, jpa 메소드 설정 다시보기
-//        MemberDTO memberDTO = mapper.toDto(m);
+        //실패 1. 수정할 멤버가 존재하지 않음
+        Member m = memberRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 ID가 존재하지 않습니다. ID : " + id));
 
-        Optional<Member> m = memberRepository.findById(id);
-        m.orElseThrow(()->new IllegalArgumentException("해당 ID가 존재하지 않습니다. ID : " + id));
+        m.update(memberDTO.getNickName(), memberDTO.getPwd());
+        memberRepository.save(m);
 
-        m.get().update(memberDTO.getNickName(), memberDTO.getPwd());
-        memberRepository.save(m.get());
-
-        return mapper.toDto(m.get());
+        return mapper.toDto(m);
     }
 
     //회원 삭제
     public void deleteMem(long id) {
 
-        Optional<Member> m = memberRepository.findById(id);
-        m.orElseThrow(()->new IllegalArgumentException("해당 ID가 존재하지 않습니다. ID : " + id));
+        Member m = memberRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 ID가 존재하지 않습니다. ID : " + id));
 
         memberRepository.deleteById(id);
     }
