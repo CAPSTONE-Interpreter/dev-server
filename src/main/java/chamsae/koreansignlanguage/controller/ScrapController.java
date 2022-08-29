ffc35e95e1046b8e2842a67f695458b1c0a890d3
zzinaa <wlsdk2769@ajou.ac.kr>
2022-08-29 15:12:00 +0900

    컨트롤러에서 exception 잡는 부분 삭제

--- src/main/java/chamsae/koreansignlanguage/controller/ScrapController.java
+++ src/main/java/chamsae/koreansignlanguage/controller/ScrapController.java
@@ -1,5 +1,6 @@
 package chamsae.koreansignlanguage.controller;
 
+import chamsae.koreansignlanguage.DTO.FindScrapsResDTO;
 import chamsae.koreansignlanguage.DTO.MemberDTO;
 import chamsae.koreansignlanguage.DTO.ScrapDTO;
 import chamsae.koreansignlanguage.DTO.VideoDTO;
@@ -37,56 +38,29 @@ public class ScrapController {
 
     @ApiOperation(value = "스크랩 조회", notes = "해당 회원의 스크랩 목록 조회합니다.")
     @GetMapping("scraps/{mem_id}")
-    public ResponseEntity getScrap(@ApiParam(value = "회원 ID", required = true) @PathVariable("mem_id") long mem_id) {
+    public ResponseEntity<FindScrapsResDTO> getScrap(@ApiParam(value = "회원 ID", required = true) @PathVariable("mem_id") long mem_id) {
         log.info("GET /scraps/{} 실행 - 해당 회원 스크랩 리스트 불러오기 : {}", mem_id, mem_id);
 
-        try {
-            Map<String, Object> result = scrapService.findByMemId(mem_id);
+        FindScrapsResDTO response = scrapService.findByMemId(mem_id);
 
-            //조회 실패
-            //1. 멤버의 스크랩이 존재하지 않음
-            if((int)result.get("count") == 0)
-                return ResponseEntity
-                        .status(HttpStatus.NOT_FOUND)
-                        .body("해당 멤버의 스크랩이 존재하지 않습니다.");
-
-            //조회 성공
-            return ResponseEntity.ok(result);
-
-        } catch (IllegalArgumentException e) {
-
-            //2. 조회하려는 멤버 아이디에 대한 멤버가 존재하지 않음
-            return ResponseEntity
-                    .status(HttpStatus.NOT_FOUND)
-                    .body("해당 멤버 아이디는 존재하지 않습니다. 잘못된 경로입니다.");
-        }
+        return ResponseEntity.ok(response);
     }
 
 
     @ApiOperation(value = "스크랩 추가", notes = "해당 회원의 스크랩 목록에 새로운 비디오를 추가합니다.")
     @PostMapping("scraps")
-    public ResponseEntity registerScrap(@ApiParam(value = "스크랩 정보", required = true) @RequestBody ScrapDTO scrapDTO) {
+    public ResponseEntity<ScrapDTO> registerScrap(@ApiParam(value = "스크랩 정보", required = true) @RequestBody ScrapDTO scrapDTO) {
         log.info("POST /scraps 실행 - 해당 스크랩 추가 (memId : {}, vidId : {})", scrapDTO.getMemId(), scrapDTO.getVidId());
 
-        try {
-            Scrap scrap = scrapService.registerScrap(scrapDTO);
+        Scrap scrap = scrapService.registerScrap(scrapDTO);
 
-            //스크랩 추가 성공
-            URI location = ServletUriComponentsBuilder.fromCurrentRequest()
-                    .path("/{id}")
-                    .buildAndExpand(scrap.getMemId())
-                    .toUri();
+        //스크랩 추가 성공
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(scrap.getMemId())
+                .toUri();
 
-            return ResponseEntity.created(location).build();
-        }
-        catch (IllegalArgumentException e) {
-            //추가 실패
-            //1. 스크랩에 추가하려는 비디오 아이디가 존재하지 않음
-            return ResponseEntity
-                    .status(HttpStatus.NOT_FOUND)
-                    .body("해당 비디오 아이디는 존재하지 않습니다.");
-        }
-
+        return ResponseEntity.created(location).build();
     }
 
 
